7ec3f74bd1dfff4ef7c7b796860b443299472dee
Lighters_c <zyh13227@163.com>
2017-12-24 19:14:17 +0800

    商品管理，到上传文件

---
+++ src/main/java/com/mmall/service/IProductService.java
@@ -0,0 +1,22 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.vo.ProductDetailVo;
+import org.springframework.stereotype.Service;
+
+@Service("iProductService")
+public interface IProductService {
+
+    ServerResponse saveOrUpdateProduct(Product product);
+
+    ServerResponse<String> setSaleStatus(Integer productId, Integer status);
+
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+
+    ServerResponse getProductList(int pageNum,int pageSize);
+
+    ServerResponse<PageInfo> searchProduct(String productName, Integer productId, int pageNum, int pageSize);
+
+}
