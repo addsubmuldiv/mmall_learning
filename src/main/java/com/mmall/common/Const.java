b19feb2d32e71585aa24f1a5819581ede7dabc5a
Lighters_c <zyh13227@163.com>
2018-01-24 19:41:50 +0800

    完成了购物车模块，并且测试通过

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -4,7 +4,7 @@ import com.google.common.collect.Sets;
 
 import java.util.Set;
 
-public class Const {
+public final class Const {
     public static final String CURRENT_USER = "currentUser";
 
     public static final String EMAIL = "email";
@@ -15,6 +15,13 @@ public class Const {
         Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc","price_asc");
     }
 
+    public interface Cart{
+        int CHECKED=1;  //购物车选中
+        int UNCHECKED=0;    //购物车未选中
+
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
+    }
 
 
     public interface Role {
